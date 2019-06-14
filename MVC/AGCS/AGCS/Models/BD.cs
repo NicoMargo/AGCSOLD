52dc3c15051ccaf477ea7771999ef1af215e1468
Android Studio <devandroid@ort>
2019-06-14 09:00:36 -0300

    xd

--- MVC/AGCS/AGCS/Models/BD.cs
+++ MVC/AGCS/AGCS/Models/BD.cs
@@ -9,6 +9,7 @@ namespace AGCS.Models
 {
     public class BD
     {
+        //xd
        //public static string connectionString = "Server=127.0.0.1;User=root;Database=pruebaclientes"; //Anush
         public static string connectionString = "Server=localhost;User=root;Database=bd_agcs"; //Ort
         public static List<Client> ListOfClients = new List<Client>();
